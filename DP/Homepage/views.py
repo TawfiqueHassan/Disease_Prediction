36e21631c48ae50d72bc50ac69057261d335c100
Tawfique Hassan <tawfique.hassan@northsouth.edu>
2023-04-05 13:42:46 +0600

    Options added

--- DP/Homepage/views.py
+++ DP/Homepage/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import pickle as pk
 
-# Create your views here.
+def index(request):
+    with open('Model','rb') as file:
+        model = pk.load(file)
+    print(model.predict([[3,4,4,7,4,3,6,5,7,5,4,0,0,0,0,0,0]]))
+    return render(request, "index.html")
